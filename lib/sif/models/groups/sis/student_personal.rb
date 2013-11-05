1abcd3242a4691c2090d751605483907de5b90a3
Kenneth Romney <kromney@instructure.com>
2013-11-05 13:17:09 -0700

    Added the rest of the fields for StudentPersonal

--- lib/sif/models/groups/sis/student_personal.rb
+++ lib/sif/models/groups/sis/student_personal.rb
@@ -10,19 +10,34 @@ module SIF
           attribute :name, String
           attribute :ref_id, String
           attribute :local_id, String
-          attribute :alert_messages, Array[Common::AlertMessage]
-          attribute :medical_alert_messages, Array[Common::MedicalAlertMessage]
           attribute :state_province_id, String
-          attribute :electronic_ids, Array[Common::ElectronicId]
-          attribute :other_ids, Array[Common::OtherId]
           attribute :other_names, String
-          attribute :demographics, String
-          attribute :addresses, Array[Common::Address]
-          attribute :phone_numbers, Array[Common::PhoneNumber]
-          attribute :emails, Array[Common::Email]
           attribute :projected_graduation_year, Integer
           attribute :on_time_graduation_year, Integer
           attribute :graduation_date, Date
+          attribute :most_recent, String
+          attribute :acceptable_use_policy, String
+          attribute :idea, String
+          attribute :migrant, String
+          attribute :title1, String
+          attribute :gifted_talented, String
+          attribute :ell, String
+          attribute :homeless, String
+          attribute :section504, String
+          attribute :vocational_concentrator, String
+          attribute :immigrant, String
+          attribute :neglected_delinquent, String
+          attribute :metadata, String
+          attribute :extended_elements, String
+
+          attribute :alert_messages, Array[Common::AlertMessage]
+          attribute :medical_alert_messages, Array[Common::MedicalAlertMessage]
+          attribute :electronic_ids, Array[Common::ElectronicId]
+          attribute :other_ids, Array[Common::OtherId]
+          attribute :addresses, Array[Common::Address]
+          attribute :phone_numbers, Array[Common::PhoneNumber]
+          attribute :emails, Array[Common::Email]
+          attribute :demographics, String
         end
       end
     end
