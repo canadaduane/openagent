eaf2e2696a8adabfb88239c46e0811a431949812
Kenneth Romney <kromney@instructure.com>
2013-11-07 13:39:15 -0700

    Created representers for SIS general objects and Personal objects.

--- lib/sif/representations/xml/models/groups/sis/student_personal.rb
+++ lib/sif/representations/xml/models/groups/sis/student_personal.rb
@@ -6,14 +6,11 @@ module SIF
           module SIS
             class StudentPersonal < Representable::Decorator
               include Representable::XML
+              include SISRepresenter
+              include SISPersonalRepresenter
 
               self.representation_wrap = 'StudentPersonal'
 
-              property :name, :as => "Name"
-              property :ref_id, :attribute => true, :as => "RefId"
-              property :local_id, :as => "LocalId"
-              property :state_province_id, :as => "StateProvinceId"
-              property :other_names, :as => "OtherName"
               property :projected_graduation_year, :as => "ProjectedGraduationYear"
               property :on_time_graduation_year, :as => "OnTimeGraduationYear"
               property :graduation_date, :as => "GraduationDate"
@@ -31,31 +28,14 @@ module SIF
               property :neglected_delinquent, :as => "NeglectedDelinquent"
               property :metadata, :as => "SIF_Metadata"
               property :extended_elements, :as => "SIF_ExtendedElements"
-
+              classmodel = SIF::Model
+              xmldecorator = SIF::Representation::XML
               collection :alert_messages, :as => "AlertMessages",
-                         :class => SIF::Model::AlertMessage,
-                         :decorator => SIF::Representation::XML::AlertMessage
+                         :class => classmodel::AlertMessage,
+                         :decorator => xmldecorator::AlertMessage
               collection :medical_alert_messages, :as => "MedicalAlertMessages",
                          :class => SIF::Model::MedicalAlertMessage,
                          :decorator => SIF::Representation::XML::MedicalAlertMessage
-              collection :electronic_ids, :as => "ElectronicIdList",
-                         :class => SIF::Model::ElectronicId,
-                         :decorator => SIF::Representation::XML::ElectronicId
-              collection :other_ids, :as => "OtherIdList",
-                         :class => SIF::Model::OtherId,
-                         :decorator => SIF::Representation::XML::OtherId
-              collection :addresses, :as => "AddressList",
-                         :class => SIF::Model::Address,
-                         :decorator => SIF::Representation::XML::Address
-              collection :phone_numbers, :as => "PhoneNumberList",
-                         :class => SIF::Model::PhoneNumber,
-                         :decorator => SIF::Representation::XML::PhoneNumber
-              collection :emails, :as => "EmailList",
-                         :class => SIF::Model::Email,
-                         :decorator => SIF::Representation::XML::Email
-              property  :demographics, :as => "Demographics",
-                        :class => SIF::Model::Demographics,
-                        :decorator => SIF::Representation::XML::Demographics
             end
           end
         end
