663f4e9a685fb94968b7cefeaff2a31fcccb9493
Eric Adams <eadams@instructure.com>
2014-01-13 11:19:06 -0700

    sif events now parse entire event data

--- lib/sif/representations/infra/common/event_object.rb
+++ lib/sif/representations/infra/common/event_object.rb
@@ -8,6 +8,27 @@ module SIF
 
           property :object_name, :as => "ObjectName", :attribute => true
           property :action, :as => "Action", :attribute => true
+          property :school_course_infos, :as => 'SchoolCourseInfo',
+                     :class => SIF::Model::Group::SIS::SchoolCourseInfo,
+                     :decorator => Model::Group::SIS::SchoolCourseInfo
+          property :school_infos, :as => 'SchoolInfo',
+                     :class => SIF::Model::Group::SIS::SchoolInfo,
+                     :decorator => Model::Group::SIS::SchoolInfo
+          property :staff_personals, :as => 'StaffPersonal',
+                     :class => SIF::Model::Group::SIS::StaffPersonal,
+                     :decorator => Model::Group::SIS::StaffPersonal
+          property :student_personals, :as => 'StudentPersonal',
+                     :class => SIF::Model::Group::SIS::StudentPersonal,
+                     :decorator => Model::Group::SIS::StudentPersonal
+          property :student_section_enrollments, :as => 'StudentSectionEnrollment',
+                     :class => SIF::Model::Group::SIS::StudentSectionEnrollment,
+                     :decorator => Model::Group::SIS::StudentSectionEnrollment
+          property :term_infos, :as => 'TermInfo',
+                     :class => SIF::Model::Group::SIS::TermInfo,
+                     :decorator => Model::Group::SIS::TermInfo
+          property :section_infos, :as => 'SectionInfo',
+                     :class => SIF::Model::Group::SIS::SectionInfo,
+                     :decorator => Model::Group::SIS::SectionInfo
         end
       end
     end
