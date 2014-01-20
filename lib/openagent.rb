7159bc7fa97040848ba7920e616153429c7614ed
Duane Johnson <duane@instructure.com>
2014-01-20 15:13:08 -0700

    Add 'repr' method
    A shortcut method that gets the Representation of
    a model and initializes it with the model.

--- lib/openagent.rb
+++ lib/openagent.rb
@@ -2,3 +2,9 @@ require 'openagent/message_builder'
 require 'openagent/zone'
 require 'openagent/agent'
 require 'openagent/client'
+
+require 'active_support/inflector'
+
+def repr(obj)
+  obj.class.to_s.sub('SIF::', 'SIF::Representation::').constantize.new(obj)
+end
