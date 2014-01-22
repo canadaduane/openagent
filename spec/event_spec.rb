e42c47be92d2a6755585a0926f89fae1d655bedf
Eric Adams <eadams@instructure.com>
2014-01-21 17:01:07 -0700

    added SIF.repr to spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,5 +1,6 @@
 require_relative "spec_helper"
 require "openagent/client"
+require "openagent"
 
 
 describe OpenAgent::Client do
@@ -29,7 +30,7 @@ describe OpenAgent::Client do
   it "creates GradingAssignment event object" do
 
     event = builder.event('GradingAssignment','Add',@grade_assignment_object_rep)
-    event_rep = SIF::Representation::Infra::Common::Message.new(event).to_xml
+    event_rep = SIF.repr(event).to_xml
     event_rep.should == "<SIF_Message xmlns=\"http://www.sifinfo.org/infrastructure/2.x\" Version=\"2.0r1\">\n  <SIF_Event>\n    <SIF_Header>\n      <SIF_MsgId>GUUID</SIF_MsgId>\n      <SIF_Timestamp>TIME</SIF_Timestamp>\n      <SIF_SourceId>canvas</SIF_SourceId>\n    </SIF_Header>\n    <SIF_ObjectData>\n      <SIF_EventObject ObjectName=\"GradingAssignment\" Action=\"Add\">\n        <GradingAssignment RefId=\"359D75101AD0A9D7A8C3DAD0A85103A2\" SectionInfoRefId=\"D0A0A27AD0A8510AD9D75101A8C3DA39\" TermInfoRefId=\"A8C3A2E35B359D75101D00AA001A0000\" SchoolInfoRefId=\"11737EA4301CADCA75C87214A7C46BDB\" GradingCategoryRefId=\"98A57A0013BA8C3D00AA012B359D751A\">\n          <Description>Pop Quiz</Description>\n          <PointsPossible>100</PointsPossible>\n          <CreateDate>2000-11-21</CreateDate>\n          <DueDate>2000-11-25</DueDate>\n          <Weight>1.0</Weight>\n          <SIF_ExtendedElements/>\n        </GradingAssignment>\n      </SIF_EventObject>\n    </SIF_ObjectData>\n  </SIF_Event>\n</SIF_Message>"
 
   end
@@ -37,15 +38,15 @@ describe OpenAgent::Client do
   it "creates GradingAssignmentScore event object" do
 
     event = builder.event('GradingAssignmentScore','Add',@grade_assignment_score_object_rep)
-    event_rep = SIF::Representation::Infra::Common::Message.new(event).to_xml
+    event_rep = SIF.repr(event).to_xml
     event_rep.should == "<SIF_Message xmlns=\"http://www.sifinfo.org/infrastructure/2.x\" Version=\"2.0r1\">\n  <SIF_Event>\n    <SIF_Header>\n      <SIF_MsgId>GUUID</SIF_MsgId>\n      <SIF_Timestamp>TIME</SIF_Timestamp>\n      <SIF_SourceId>canvas</SIF_SourceId>\n    </SIF_Header>\n    <SIF_ObjectData>\n      <SIF_EventObject ObjectName=\"GradingAssignmentScore\" Action=\"Add\">\n        <GradingAssignmentScore StudentPersonalRefId=\"A75A00101A8C301D02E3A05B359D0A00\" SectionInfoRefId=\"D0A0A27AD0A8510AD9D75101A8C3DA39\" GradingAssignmentRefId=\"359D75101AD0A9D7A8C3DAD0A85105D2\" SchoolInfoRefId=\"11737EA4301CADCA75C87214A7C46BDB\">\n          <ScorePoints>45</ScorePoints>\n          <ScorePercent>90</ScorePercent>\n          <ScoreDescription>Excellent</ScoreDescription>\n          <SIF_ExtendedElements/>\n        </GradingAssignmentScore>\n      </SIF_EventObject>\n    </SIF_ObjectData>\n  </SIF_Event>\n</SIF_Message>"
 
   end
 
   it "creates GradingCategory event object" do
-    
+
     event = builder.event('GradingCategory','Add',@grade_category_object_rep)
-    event_rep = SIF::Representation::Infra::Common::Message.new(event).to_xml
+    event_rep = SIF.repr(event).to_xml
     event_rep.should == "<SIF_Message xmlns=\"http://www.sifinfo.org/infrastructure/2.x\" Version=\"2.0r1\">\n  <SIF_Event>\n    <SIF_Header>\n      <SIF_MsgId>GUUID</SIF_MsgId>\n      <SIF_Timestamp>TIME</SIF_Timestamp>\n      <SIF_SourceId>canvas</SIF_SourceId>\n    </SIF_Header>\n    <SIF_ObjectData>\n      <SIF_EventObject ObjectName=\"GradingCategory\" Action=\"Add\">\n        <GradingCategory RefId=\"98157AA013BA8C3D00AA012B359D7512\" SectionInfoRefId=\"13BA8C3D00AA098157AA012B359D7512\" TermInfoRefId=\"D00AA012B359D798157AA013BA8C3512\" SchoolInfoRefId=\"11737EA4301CADCA75C87214A7C46BDB\">\n          <Description>Tests</Description>\n          <SIF_ExtendedElements/>\n        </GradingCategory>\n      </SIF_EventObject>\n    </SIF_ObjectData>\n  </SIF_Event>\n</SIF_Message>"
 
   end
